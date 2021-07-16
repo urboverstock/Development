f1b6a0af2671501f445d1c0756b7ee0b3ab4776c
Raman <jotsaggu@gmail.com>
2021-07-16 23:33:02 +0530

    update migrations

--- database/migrations/2021_04_30_155061_create_orders_table.php
+++ database/migrations/2021_04_30_155061_create_orders_table.php
@@ -34,8 +34,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('orders');
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
