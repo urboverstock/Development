bb392272e285669fa6eae995755b399542882e31
Raman <jotsaggu@gmail.com>
2021-07-16 23:01:01 +0530

    update migrations

--- database/migrations/2014_10_12_000006_create_chats_table.php
+++ database/migrations/2014_10_12_000006_create_chats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatRoomTable extends Migration
+class CreateChatsTable extends Migration
 {
     /**
      * Run the migrations.
