237ffb047a383b417fa84b4463881cb34a0c8660
Raman <jotsaggu@gmail.com>
2021-09-13 00:00:58 +0530

    update socket index file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('http').createServer(app);
 
 
 const io = require('socket.io')(server, {
-  cors : { origin: "*" }
+  cors : { origin: "*" ,reconnect: true, rejectUnauthorized : false}
 });
 
 var usernames = [];
