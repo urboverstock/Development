5b06162e33d5d323f9f1ecc540891211a40990f3
Raman <jotsaggu@gmail.com>
2021-09-12 23:34:24 +0530

    update cors in node

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 // const http = require('http');
+// const server = require('http').createServer(app);
 const server = require('https').createServer(app);
 
 
 const io = require('socket.io')(server, {
-  cors : { origin: "*" }
+  cors : { origins: ["*"],
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methos": "GET,POST"
+      })
+    }
+  }
 });
 
 var usernames = [];
